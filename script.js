44fee86d453f35ae192bbbe9046728b80d2a8897
zeelprajapati1 <zeelprajapati9876@gmail.com>
2025-03-26 02:47:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+// Create the student object
+const student = {
+  name: "John",
+};
+
+// Function to get all keys from the object
+function getKeys(obj) {
+  return Object.keys(obj);
+}
+
+// Testing the function
+console.log(getKeys(student)); // Output: ["name"]
+
+// Do not change the code below this line
+window.student = student;
+window.getKeys = getKeys;
